43899d18e8d91e52995412a70181b422e8169c72
Lexxoi <alexxtkd@gmail.com>
2026-04-01 23:19:41 +0300

    rollback test

--- modules/mapicons.lua
+++ modules/mapicons.lua
@@ -197,21 +197,21 @@ function FlightTracker_MapIcon_OnEnter(button)
                 duration = FlightTrackerDB.flights[reverseKey]
             end
 
-            local isEstimated = false
-            if not duration then
-                duration = FlightTracker:GetEstimatedFlightTime(origin, button.nodeName)
-                if duration then isEstimated = true end
-            end
+local isEstimated = false
+if not duration then
+    duration = FlightTracker:GetEstimatedFlightTime(origin, button.nodeName)
+    if duration then isEstimated = true end
+end
 
-            if duration then
-                if isEstimated then
-                    GameTooltip:AddLine("Estimated: " .. FlightTracker.Util.FormatTime(duration), 0.4, 0.8, 1)
-                else
-                    GameTooltip:AddLine("Flight time: " .. FlightTracker.Util.FormatTime(duration), 1, 1, 1)
-                end
-            else
-                GameTooltip:AddLine("No flight time recorded yet", 0.5, 0.5, 0.5)
-        end
+if duration then
+    if isEstimated then
+        GameTooltip:AddLine("Estimated: " .. FlightTracker.Util.FormatTime(duration), 0.4, 0.8, 1)
+    else
+        GameTooltip:AddLine("Flight time: " .. FlightTracker.Util.FormatTime(duration), 1, 1, 1)
+    end
+else
+    GameTooltip:AddLine("No flight time recorded yet", 0.5, 0.5, 0.5)
+end
 
         -- Draw only true direct lines (gryphon-master style)
         ShowLinesForNode(button.nodeName)
